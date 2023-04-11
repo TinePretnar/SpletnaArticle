e73a5effa678d5aef9afd5f2a4e075247afa4733
TinePretnar <73723936+TinePretnar@users.noreply.github.com>
2023-04-11 15:12:51 +0200

    yep

--- src/main/java/com/example/SpletnaArticle/ArticleController.java
+++ src/main/java/com/example/SpletnaArticle/ArticleController.java
@@ -1,11 +1,14 @@
 package com.example.SpletnaArticle;
-import org.springframework.ui.Model;
 import com.example.SpletnaArticle.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.SpletnaArticle.service.ArticleService;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/")
@@ -18,14 +21,27 @@ public class ArticleController {
         List<Article> articles = articleService.getAllArticles();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("articles", articles);
-        modelAndView.setViewName("articleList");
+        modelAndView.setViewName("index");
         return modelAndView;
     }
 
     @GetMapping("/{id}")
-    public String getArticleById(@PathVariable Long id) {
+    public String getArticleById(@PathVariable int id) {
         return "Article with ID " + id;
     }
+
+    /*@GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = new ClassPathResource(filename);
+        if (file.exists()) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getFilename() + "\"")
+                    .contentType(MediaTypeFactory.getMediaType(file).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                    .body(file);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }*/
 
     @PostMapping
     public String createArticle(@RequestBody Article article) {
@@ -33,19 +49,13 @@ public class ArticleController {
     }
 
     @PutMapping("/{id}")
-    public String updateArticle(@PathVariable Long id, @RequestBody Article article) {
+    public String updateArticle(@PathVariable int id, @RequestBody Article article) {
         return "Article with ID " + id + " updated: " + article.getTitle();
     }
 
     @DeleteMapping("/{id}")
-    public String deleteArticle(@PathVariable Long id) {
+    public String deleteArticle(@PathVariable int id) {
         return "Article with ID " + id + " deleted";
     }
 
-    @GetMapping("/articles")
-    public String getAllArticles(Model model) {
-        List<Article> articles = articleService.getAllArticles();
-        model.addAttribute("articles", articles);
-        return "articles";
-    }
 }
